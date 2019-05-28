271860d744c72039e43ba11122158522d84bde14
bennyp1331 <ben.poynor@gmail.com>
2019-05-27 19:04:06 -0600

    volume machine broke

--- backtesting/backtest.py
+++ backtesting/backtest.py
@@ -1,32 +1,43 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from mpl_finance import candlestick2_ochl
-import backtesting.randomdata as rd
-import backtesting.mkdata as mk
-import backtesting.importdata as id
+from mpl_finance import candlestick2_ochl, volume_overlay2
+try:
+    import backtesting.datacontroller as dc
+except ImportError:
+    import datacontroller as dc
 
 
 class Backtest:
     dataset = None
 
     def __init__(self, ds):
-        Backtest.dataset = ds
+        Backtest.dataset = pd.DataFrame(data=ds)
 
     def graph(self):
-        f1 = plt.subplot2grid((6, 4), (1, 0), rowspan=6, colspan=4)
-        candlestick2_ochl(ax=f1,
+        fig, ax1 = plt.subplots()
+        ax1.set_xlabel('Date')
+        ax1.set_ylabel('Price')
+        ax2 = ax1.twinx()
+        ax2.set_ylabel('Volume')
+        candlestick2_ochl(ax=ax1,
                           opens=Backtest.dataset['open'],
                           closes=Backtest.dataset['close'],
                           highs=Backtest.dataset['high'],
                           lows=Backtest.dataset['low'],
                           width=1,
                           colorup='green',
-                          colordown='red')
+                          colordown='red',
+                         )
+        volume_overlay2(ax=ax2,
+                        closes=Backtest.dataset['close'],
+                        volumes=Backtest.dataset['volume'],
+                        width=1,
+                        alpha=.5,
+                        colorup='green',
+                        colordown='red')
         plt.show()
 
-x = mk.get_price_history('MSFT', '2019-01-01', '2019-02-01')
-print(x.head())
 
-pricedata = pd.DataFrame(data=rd.fake_data(30))
-bt = Backtest(pricedata)
+raw_data = dc.get_debug_pricehist('BTC-USD', '2019-04-01', '2019-05-26')
+bt = Backtest(raw_data)
 bt.graph()
