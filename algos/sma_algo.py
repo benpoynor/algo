2a332b300f86a9ddc9829271f93cfca95ad080a8
bennyp1331 <ben.poynor@gmail.com>
2019-06-17 18:21:06 -0600

    added capital allocation to algorithm

--- algos/sma_algo.py
+++ algos/sma_algo.py
@@ -4,6 +4,9 @@ from utilities.technicals import Technicals
 
 class MovingAverageAlgo(Algorithm):
     def __init__(self):
+        self.capital = 15000
+        self.position_sizing = 1
+        self.safety_margin = 100
         super().__init__()
 
     # TODO: add runtime type param that alters the action if backtesting or on live etc..
@@ -12,9 +15,16 @@ class MovingAverageAlgo(Algorithm):
     def action(self, index, data):
         if Technicals.sma(20, data, index) < Technicals.sma(50, data, index):
             if not self.position.is_open:
-                self.position.open(1, 'BTC-USD', data[index].get('close'), data[index].get('formatted_date'))
+                self.position.open(self.get_buy_amount(float(data[index].get('close'))),
+                                   'BTC-USD',
+                                   data[index].get('close'),
+                                   data[index].get('formatted_date'))
+
         elif Technicals.sma(20, data, index) > Technicals.sma(50, data, index):
             if self.position.is_open:
-                self.position.close(data[index].get('close'), data[index].get('formatted_date'))
+                self.position.close(data[index].get('close'),
+                                    data[index].get('formatted_date'))
 
         self.update_alpha()
+        self.update_capital()
+        self.reset_alpha()
