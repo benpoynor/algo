f2720209654814086881f99666e7a2e32b335439
bennyp1331 <ben.poynor@gmail.com>
2019-09-04 14:15:31 -0600

    starting work on production environment

---
+++ prod.py
@@ -0,0 +1,29 @@
+# https://docs.gemini.com/rest-api/#introduction
+
+
+class LiveDemo:
+    def __init__(self):
+        self.api_public_key = None
+        self.api_private_key = None
+
+    def test_buy(self):
+        # buy from gemini
+        pass
+
+    def test_sell(self):
+        # sell from gemini
+        pass
+
+    def test_run(self):
+        # run with an algo
+        pass
+
+    
+class Production:
+    def __init__(self):
+        self.api_public_key = None
+        self.api_private_key = None
+
+    def run(self):
+        # start live trading with an algo
+        pass
